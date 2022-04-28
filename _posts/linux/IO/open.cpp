169bfbeb876ef3f1322b8f08ae2bd33a357199d0
pokerjest <pokerjest@outlook.com>
2022-04-28 10:22:00 +0800

    Revert "temp"
    This reverts commit e26bafe9e06e9a590595206c6d764baa5b286729.

--- _posts/linux/IO/open.cpp
+++ _posts/linux/IO/open.cpp
@@ -3,8 +3,7 @@
 #include <stdio.h>
 int main(void)
 {
-    int fd = open("hello.in", 
-    O_RDWR|O_CREAT|O_TRUNC|O_SYNC|O_NONBLOCK, S_IRUSR|S_IWUSR);
+    int fd = open("hello.in", O_RDWR|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
     printf("hello\n");
 
     return 0;
